4f6a882c05e88d564b9a777215afbaf9c49e7f2a
Tim Huddle <blazhen88@gmail.com>
2021-03-13 20:19:20 -0500

    wire up the whole settings drawer

--- lib/calunedar.dart
+++ lib/calunedar.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 
-import 'package:calunedar/settings.dart';
 import 'package:calunedar/widgets/app_bar.dart';
 import 'package:calunedar/widgets/month.dart';
 import 'package:calunedar/widgets/month_info.dart';
@@ -8,7 +7,6 @@ import 'package:calunedar/widgets/month_readout.dart';
 import 'package:calunedar/widgets/settings_drawer.dart';
 import 'package:dart_date/dart_date.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class Calunedar extends StatefulWidget {
   @override
@@ -20,48 +18,46 @@ class _CalunedarState extends State<Calunedar> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<Settings>(builder: (context, settings, child) {
-      final monthInfo = MonthInfo(date: _date);
-      final media = MediaQuery.of(context);
+    final monthInfo = MonthInfo(date: _date);
+    final media = MediaQuery.of(context);
 
-      return Scaffold(
-        drawer: SettingsDrawer(settings: settings),
-        appBar: CalendarAppBar(
-          date: _date,
-          setDate: (DateTime newDate) {
-            this.setState(() {
-              _date = newDate;
-            });
-          },
-        ),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 15.0,
-                vertical: 15.0,
-              ),
-              width: min(media.size.width, 1024.0),
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Month(date: _date, monthInfo: monthInfo),
-                    Divider(
-                      color: Colors.black,
-                      thickness: 1.0,
-                      height: 30.0,
-                    ),
-                    MonthReadout(monthInfo: monthInfo),
-                  ],
-                ),
+    return Scaffold(
+      drawer: SettingsDrawer(),
+      appBar: CalendarAppBar(
+        date: _date,
+        setDate: (DateTime newDate) {
+          this.setState(() {
+            _date = newDate;
+          });
+        },
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 15.0,
+              vertical: 15.0,
+            ),
+            width: min(media.size.width, 1024.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Month(date: _date, monthInfo: monthInfo),
+                  Divider(
+                    color: Colors.black,
+                    thickness: 1.0,
+                    height: 30.0,
+                  ),
+                  MonthReadout(monthInfo: monthInfo),
+                ],
               ),
             ),
-          ],
-        ),
-      );
-    });
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/widgets/settings_drawer.dart
+++ lib/widgets/settings_drawer.dart
@@ -1,29 +1,100 @@
 import 'package:calunedar/settings.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:o_color_picker/o_color_picker.dart';
+import 'package:provider/provider.dart';
 
 class SettingsDrawer extends StatelessWidget {
-  SettingsDrawer({this.settings});
-
-  final Settings settings;
-
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-            child: Text('Settings Menu'),
+    return Consumer<Settings>(builder: (context, settings, children) {
+      return Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Settings Menu'),
+            ),
+            ..._buildCalendar(
+              settings: settings,
+            ),
+            _buildThemeColor(
+              settings: settings,
+              context: context,
+            ),
+          ],
+        ),
+      );
+    });
+  }
+
+  List<Widget> _buildCalendar({@required Settings settings}) {
+    final calendarList = <Widget>[
+      ListTile(
+        leading: Text('Calendar: '),
+        title: DropdownButton<CalendarType>(
+          value: settings.calendar,
+          onChanged: (CalendarType newValue) {
+            settings.calendar = newValue;
+          },
+          isDense: true,
+          items: CalendarType.values
+              .map(
+                (e) => DropdownMenuItem(
+                  child: Text(describeEnum(e)[0] +
+                      describeEnum(e).substring(1).toLowerCase()),
+                  value: e,
+                ),
+              )
+              .toList(),
+        ),
+      ),
+    ];
+
+    if (settings.calendar != CalendarType.GREGORIAN) {
+      calendarList.add(ListTile(
+        title: Icon(!settings.metonic
+            ? Icons.check_circle_outline
+            : Icons.check_circle),
+        leading: Text('Metonic: '),
+        onTap: () => {settings.metonic = !settings.metonic},
+      ));
+    }
+
+    return calendarList;
+  }
+
+  Widget _buildThemeColor({Settings settings, BuildContext context}) {
+    return ListTile(
+      leading: Text('Primary Theme Color: '),
+      title: TextButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.resolveWith(
+            (states) => settings.primaryColor,
           ),
-          ListTile(
-            leading: Icon(!settings.metonic
-                ? Icons.check_circle_outline
-                : Icons.check_circle),
-            title: Text('Metonic'),
-            onTap: () => {settings.metonic = !settings.metonic},
+        ),
+        child: Container(),
+        onPressed: () => showDialog(
+          context: context,
+          builder: (_) => Dialog(
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              child: OColorPicker(
+                boxBorder: OColorBoxBorder(
+                  color: Colors.black,
+                  radius: 5.0,
+                ),
+                selectedColor: settings.primaryColor,
+                colors: primaryColorsPalette,
+                onColorChange: (color) {
+                  settings.primaryColor = color;
+                  Navigator.of(context).pop();
+                },
+              ),
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
